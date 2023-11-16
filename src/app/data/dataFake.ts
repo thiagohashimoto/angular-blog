e8adbfcd8f9ac60392044afb6c88c3e5f7275411
PC Thiago <thiago.hashimoto94@gmail.com>
2023-11-16 18:37:22 -0300

    Responsive with some new pages

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+    {
+        "id":1,
+        "photo":"link",
+        "title":"saiu novo patch",
+        "description":"descrição do bomba patch"
+    },
+    {
+        "id":2,
+        "photo":"link2",
+        "title":"saiu novo patch 2",
+        "description":"descrição do bomba patch 2"
+    }
+]
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent {
+  photoCover:string="https://cdn.fs.teachablecdn.com/UWKQZYkSCahRe0KPwKD8"
+  contentTitle:string="Notícia exemplo do Revit"
+  contentDescription:string="Ta cheio de novidade galera, só chega vambora"
 
 }
