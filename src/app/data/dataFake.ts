68dc317c1e1add1d1fbdea920c64f35dd9d39be9
PC Thiago <thiago.hashimoto94@gmail.com>
2023-11-19 17:52:12 -0300

    Turning pages responsive

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,12 +1,12 @@
 export const dataFake = [
     {
-        "id":1,
+        "id":"1",
         "photo":"link",
         "title":"saiu novo patch",
         "description":"descrição do bomba patch"
     },
     {
-        "id":2,
+        "id":"2",
         "photo":"link2",
         "title":"saiu novo patch 2",
         "description":"descrição do bomba patch 2"
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from '../../data/dataFake'
 
 @Component({
   selector: 'app-content',
@@ -9,5 +11,17 @@ export class ContentComponent {
   photoCover:string="https://cdn.fs.teachablecdn.com/UWKQZYkSCahRe0KPwKD8"
   contentTitle:string="Notícia exemplo do Revit"
   contentDescription:string="Ta cheio de novidade galera, só chega vambora"
+  private id:string | null = "0"
+
+  constructor(private route:ActivatedRoute
+    ){  }
+
+    ngOnInit(): void {
+      this.route.paramMap.subscribe( value => this.id = value.get("Id"))
+    }
+
+    setValuestoComponent(id:string) {
+        const result = dataFake.filter(article => article.id == id)
+    }
 
 }
